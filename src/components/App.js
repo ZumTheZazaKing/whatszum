209f39eaf725b65b04cca124d81bf6fb1cb9ec14
ZumTheZazaKing <zahidi85543@gmail.com>
2021-12-16 23:32:48 +0800

    Themes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useState } from 'react';
 import { auth } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { MainContext } from '../contexts/MainContext';
@@ -8,13 +8,30 @@ const Main = lazy(() => import('./Main').then(module => ({default:module.Main}))
 
 function App() {
 
+  const themes = {
+    light: {
+      backgroundColor1: '#fff',
+      backgroundColor2: '#f2f2f2',
+      backgroundColor3: '#284bbd',
+      textColor: '#000'
+    },
+    dark: {
+      backgroundColor1: '#212121',
+      backgroundColor2: '#303030',
+      backgroundColor3: '#1a1a1a',
+      textColor1: '#fff',
+      textColor2: 'grey'
+    }
+  }
+
   const [user, loading] = useAuthState(auth);
+  const [theme, setTheme] = useState(themes.light);
 
   return (
     <div className="App">
       <Suspense fallback={<div className='loading'><h1>Loading...</h1></div>}>
         <MainContext.Provider value={{
-          user
+          user, theme, themes, setTheme
         }}>
 
           {loading ? <div className="loading"><h1>Checking...</h1></div> : 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { MainContext } from '../contexts/MainContext';
 import { auth, db } from '../firebase';
-import { onSnapshot, doc, setDoc, serverTimestamp } from 'firebase/firestore';
+import { onSnapshot, doc, setDoc, serverTimestamp, updateDoc } from 'firebase/firestore';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -8,11 +9,14 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import LogoutIcon from '@mui/icons-material/Logout';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import '../styles/Main.css';
 
 export const Main = () => {
 
     const [userInfo, setUserInfo] = useState({});
+    const { theme, themes, setTheme } = useContext(MainContext);
 
     useEffect(() => {
         let userDoc = doc(db,"users",auth.currentUser.uid);
@@ -21,7 +25,9 @@ export const Main = () => {
                 handleNewUser(userDoc);
             }
             setUserInfo(snapshot.data());
+            setTheme(snapshot.data().isDark ? themes.dark : themes.light);
         })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[]);
 
     const handleNewUser = async userDoc => {
@@ -29,7 +35,8 @@ export const Main = () => {
             name: auth.currentUser.displayName,
             avatar: auth.currentUser.photoURL,
             description:"No description yet",
-            joinedAt:serverTimestamp()
+            joinedAt:serverTimestamp(),
+            isDark:false
         }
         await setDoc(userDoc, userPayload);
     }
@@ -38,6 +45,10 @@ export const Main = () => {
         menuHandleClose();
         auth.signOut()
         .catch(error => {return})
+    }
+    const switchTheme = async () => {
+        menuHandleClose();
+        await updateDoc(doc(db,"users",auth.currentUser.uid), {isDark:!userInfo.isDark});
     }
 
     const [menuAnchorEl, setMenuAnchorEl] = useState(null);
@@ -49,9 +60,9 @@ export const Main = () => {
         setMenuAnchorEl(null);
     };
 
-    return (<div id="main">
-        <div id="main-sidebar">
-            <div id='main-topbar'>
+    return (<div id="main" style={{backgroundColor:theme.backgroundColor2}}>
+        <div id="main-sidebar" style={{backgroundColor:theme.backgroundColor1}}>
+            <div id='main-topbar' style={{backgroundColor:theme.backgroundColor3}}>
                 <Avatar src={userInfo ? userInfo.avatar : ""} alt={userInfo ? userInfo.name : ""}/>
                 <IconButton onClick={menuHandleClick}>
                     <MoreVertIcon sx={{color:"white"}} />
@@ -74,10 +85,17 @@ export const Main = () => {
                     horizontal: 'right',
                 }}
             >
+                <MenuItem onClick={switchTheme}>
+                    <ListItemIcon>
+                        {userInfo.isDark ? <LightModeIcon fontSize="small"/> : <DarkModeIcon fontSize="small"/>}
+                    </ListItemIcon>
+                    {userInfo.isDark ? "Light Mode" : "Dark Mode"}
+                </MenuItem>
                 <MenuItem onClick={signOut}>
                     <ListItemIcon><LogoutIcon fontSize="small"/></ListItemIcon>
                     Logout
                 </MenuItem>
+                
             </Menu>
 
 
