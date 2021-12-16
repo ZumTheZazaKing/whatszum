ee80132347278d2cc981927f7c39d6bfc4980c87
ZumTheZazaKing <zahidi85543@gmail.com>
2021-12-16 18:37:48 +0800

    Added styles and handle new users

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import { lazy, Suspense } from 'react';
 import { auth } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { MainContext } from '../contexts/MainContext';
 
 const SignIn = lazy(() => import('./SignIn').then(module => ({default:module.SignIn})));
 const Main = lazy(() => import('./Main').then(module => ({default:module.Main})));
 
 function App() {
 
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
   return (
     <div className="App">
       <Suspense fallback={<div className='loading'><h1>Loading...</h1></div>}>
-        {user ? <Main /> : <SignIn />}
+        <MainContext.Provider value={{
+          user
+        }}>
+
+          {loading ? <div className="loading"><h1>Checking...</h1></div> : 
+          (user ? 
+          <Main/>:
+          <SignIn/>)}
+
+        </MainContext.Provider>
       </Suspense>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,92 @@
-import { auth } from '../firebase';
+import { useState, useEffect } from 'react';
+import { auth, db } from '../firebase';
+import { onSnapshot, doc, setDoc, serverTimestamp } from 'firebase/firestore';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import LogoutIcon from '@mui/icons-material/Logout';
+import '../styles/Main.css';
 
 export const Main = () => {
 
+    const [userInfo, setUserInfo] = useState({});
+
+    useEffect(() => {
+        let userDoc = doc(db,"users",auth.currentUser.uid);
+        onSnapshot(userDoc, snapshot => {
+            if(!snapshot.exists()){
+                handleNewUser(userDoc);
+            }
+            setUserInfo(snapshot.data());
+        })
+    },[]);
+
+    const handleNewUser = async userDoc => {
+        const userPayload = {
+            name: auth.currentUser.displayName,
+            avatar: auth.currentUser.photoURL,
+            description:"No description yet",
+            joinedAt:serverTimestamp()
+        }
+        await setDoc(userDoc, userPayload);
+    }
+
     const signOut = () => {
+        menuHandleClose();
         auth.signOut()
         .catch(error => {return})
     }
 
+    const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+    const menuOpen = Boolean(menuAnchorEl);
+    const menuHandleClick = (event) => {
+        setMenuAnchorEl(event.currentTarget);
+    };
+    const menuHandleClose = () => {
+        setMenuAnchorEl(null);
+    };
+
     return (<div id="main">
-        <h1>Main</h1>
-        <button onClick={signOut}>Sign Out</button>
+        <div id="main-sidebar">
+            <div id='main-topbar'>
+                <Avatar src={userInfo ? userInfo.avatar : ""} alt={userInfo ? userInfo.name : ""}/>
+                <IconButton onClick={menuHandleClick}>
+                    <MoreVertIcon sx={{color:"white"}} />
+                </IconButton>
+            </div>
+            <Menu
+                id="basic-menu"
+                anchorEl={menuAnchorEl}
+                open={menuOpen}
+                onClose={menuHandleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <MenuItem onClick={signOut}>
+                    <ListItemIcon><LogoutIcon fontSize="small"/></ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+
+
+            <div id="main-chats"></div>
+
+        </div>
+
+
+        <div id="main-chat-interface"></div>
+
     </div>)
 }
